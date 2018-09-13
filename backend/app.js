a2614e84d4b0b557ef36350fa707827eaec860b2
unknown <edwardnatividad@gmail.com>
2018-09-13 17:10:03 -0400

    completed node server changes and integrations

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
-
+const bodyPasrser = require('body-parser');
 const app = express();
+
+app.use(bodyPasrser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods','GET,POST,PATCH,DELETE,OPTIONS' );
   next();
+});
+
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({message:'Post added successfully'});
+
 });
 
 app.use('/api/posts',(req, res, next) =>{
